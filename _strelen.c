5821dd6b42818c4f44659431e37b8e0473c80512
AMIRANDA9112 <andreselfm@gmail.com>
2020-04-16 10:43:48 -0500

    auxiliar functions

---
+++ _strelen.c
@@ -0,0 +1,20 @@
+#include "header.h"
+/**
+ * _strlen - Entry point
+ * @s: pointer
+ * Description
+ * Return: 1 - Always success
+ */
+
+int _strlen(char *s)
+{
+        int i;
+        int a;
+
+        for (i = 0; s[a] != '\0'; i++)
+        {
+                a = i;
+        }
+        return (a);
+}
+
