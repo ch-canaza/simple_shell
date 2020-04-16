729a50340ceb3bfd8467a5a5c06d2d973c81f4f8
ch-canaza <ch-canaza@hotmail.com>
2020-04-16 12:16:10 -0500

    update simple_shell

--- simple_shell.c
+++ simple_shell.c
@@ -34,8 +34,8 @@ int main(int ac, char *ar[], char *envp[])
 		{	free(buffer);
 			exit(0);
 		}
-		if (_strcmp(buffer, "env\n") == 0)
-			print_e(envp);
+		/*if (_strcmp(buffer, "env\n") == 0)
+		  print_e(envp);*/
 		_split(buffer, av);
 		if ((*buffer != '\n') && av[0])
 			_exec(av);
